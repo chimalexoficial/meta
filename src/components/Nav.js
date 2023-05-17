66843c13e083e8490caa4e25a39e22aad27fa9a8
chimalexoficial <you@example.com>
2023-05-17 16:52:29 -0600

    components created and meta tags added

---
+++ src/components/Nav.js
@@ -0,0 +1,12 @@
+
+function Nav() {
+    return <nav>
+        <ul>
+            <li><a href="#">Page1</a></li>
+            <li><a href="#">Page2</a></li>
+            <li><a href="#">Page3</a></li>
+        </ul>
+    </nav>
+}
+
+export default Nav;
